48c81bc96af7478aaa8222280b816b54609d43c9
sotomita <so.tomita35135@gmail.com>
2025-09-24 01:29:54 +0900

    rename Python scripts

--- run/plot_u_init.py
+++ run/plot_u_init.py
@@ -4,15 +4,21 @@ import os
 import xarray as xr
 import matplotlib.pyplot as plt
 
+data_fpath = "./data/init.nc"
 fig_dir = "./fig/init"
-fpath = "./data/init.nc"
+
 
 os.makedirs(fig_dir, exist_ok=True)
-ds = xr.open_dataset(fpath)
+ds = xr.open_dataset(data_fpath)
 print(ds)
 
 fig, ax = plt.subplots(1, 1)
-cf = ax.pcolormesh(ds["u"])
+ax.set_title(f"initial u")
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+
+cf = ax.pcolormesh(ds["x"], ds["y"], ds["u"], vmin=-1, vmax=1, cmap="bwr")
+
 plt.colorbar(cf)
 plt.savefig(f"{fig_dir}/init.png")
 plt.close()
