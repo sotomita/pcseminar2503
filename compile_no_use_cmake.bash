1c4cd0e13da2686e4eb56066e8ad7d51f8fb6f83
sotomita <so.tomita35135@gmail.com>
2025-10-09 11:57:55 +0900

    [fix] cmake shared lib settings

--- compile_no_use_cmake.bash
+++ compile_no_use_cmake.bash
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=./lib:$LD_LIBRARY_PATH
-export NETCDF="./lib"
-
 mkdir -p build/bin
 
 # -----------------------------
 # init_sine
 # -----------------------------
 gfortran -O3 -I./include -c src/init_sine.f90 -o build/init_sine.o
-gfortran build/init_sine.o -o build/bin/init_sine -L$NETCDF -lnetcdff -lnetcdf
-# gfortran -O3 -I./include src/init_sine.f90 -o build/bin/init_sine -L$NETCDF -lnetcdff -lnetcdf
+gfortran build/init_sine.o -o build/bin/init_sine -L./ -lnetcdf -lnetcdff -Wl,-rpath=lib
+# gfortran -O3 -I./include src/init_sine.f90 -o build/bin/init_sine -L$N./ -lnetcdf -lnetcdff -Wl,-rpath=./lib
 
 
 # -----------------------------
@@ -18,5 +15,5 @@ gfortran build/init_sine.o -o build/bin/init_sine -L$NETCDF -lnetcdff -lnetcdf
 # -----------------------------
 gfortran -O3 -I./include -c src/read_config.f90 -o build/read_config.o
 gfortran -O3 -I./include -c src/forward.f90     -o build/forward.o
-gfortran build/read_config.o build/forward.o -o build/bin/forward -L$NETCDF -lnetcdff -lnetcdf
-# gfortran -O3 -I./include src/read_config.f90 src/forward.f90 -o build/bin/forward -L$NETCDF -lnetcdff -lnetcdf
+gfortran build/read_config.o build/forward.o -o build/bin/forward -L./ -lnetcdf -lnetcdff -Wl,-rpath=lib
+# gfortran -O3 -I./include src/read_config.f90 src/forward.f90 -o build/bin/forward -L$./ -lnetcdf -lnetcdff -Wl,-rpath=./lib
